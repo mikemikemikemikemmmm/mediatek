e3e56fd490e7db046c8f4c4608103a857d5588e1
mike <mikem40099@gmail.com>
2023-04-14 22:54:23 +0800

    delete testid when build

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,26 @@
 /// <reference types="vitest" />
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  test: {
-    environment: 'jsdom'
+export default defineConfig(({ command }) => {
+  if (command === 'serve') {
+    return {
+      plugins: [react()],
+      test: {
+        environment: 'jsdom'
+      }
+    }
+  } else {
+    return {
+      plugins: [react({
+        babel: {
+          plugins: [
+            ["react-remove-properties", { "properties": ["data-testid"] }]
+          ]
+        }
+      })],
+      test: {
+        environment: 'jsdom'
+      }
+    }
   }
 })
